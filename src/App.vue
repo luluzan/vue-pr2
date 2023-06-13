de287ecd3f2d005ad21671f4ee322a17d2b58218
keroplas <kerotupy@hotmail.com>
2023-06-13 15:07:35 +0200

    subida de footer

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,13 @@
 <script setup>
-import { RouterLink, RouterView } from 'vue-router'
-import HelloWorld from './components/HelloWorld.vue'
+// import { RouterLink, RouterView } from 'vue-router'
+// import HelloWorld from './components/HelloWorld.vue'
+import ComponentButtonFooter from './components/ComponentButtonFooter.vue'
+import ComponentFooter from './components/ComponentFooter.vue'
+
 </script>
 
 <template>
-  <header>
+  <!-- <header>
     <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
 
     <div class="wrapper">
@@ -17,44 +20,14 @@ import HelloWorld from './components/HelloWorld.vue'
     </div>
   </header>
 
-  <RouterView />
+  <RouterView /> -->
+
+  <ComponentFooter></ComponentFooter>
+  <ComponentButtonFooter></ComponentButtonFooter>
 </template>
 
 <style scoped>
-header {
-  line-height: 1.5;
-  max-height: 100vh;
-}
 
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
-
-nav {
-  width: 100%;
-  font-size: 12px;
-  text-align: center;
-  margin-top: 2rem;
-}
-
-nav a.router-link-exact-active {
-  color: var(--color-text);
-}
-
-nav a.router-link-exact-active:hover {
-  background-color: transparent;
-}
-
-nav a {
-  display: inline-block;
-  padding: 0 1rem;
-  border-left: 1px solid var(--color-border);
-}
-
-nav a:first-of-type {
-  border: 0;
-}
 
 @media (min-width: 1024px) {
   header {
@@ -81,5 +54,5 @@ nav a:first-of-type {
     padding: 1rem 0;
     margin-top: 1rem;
   }
-}
+} 
 </style>
---
+++ src/components/ComponentButtonFooter.vue
@@ -0,0 +1,109 @@
+
+<template>
+
+<section class="container-feedback">
+
+<div class="feedback">
+
+  <div class="happy-users">
+    <i class='fas fa-users'></i>
+    <p class="number">231</p>
+    <p class="p-feedback">Happy Users</p>
+  </div>
+
+  <div class="issues-solved">
+    <i class='fas fa-code'></i>
+    <p class="number">385</p>
+    <p class="p-feedback">Issues Solved</p>
+  </div>
+
+  <div class="good-reviews">
+    <i class='fas fa-gear'></i>
+    <p class="number">159</p>
+    <p class="p-feedback">Good Reviews</p>
+  </div>
+
+  <div class="case-studies">
+    <i class='fas fa-comments'></i>
+    <p class="number">127</p>
+    <p class="p-feedback">Case Studies</p>
+  </div>
+
+  <div class="orders-received">
+    <i class='fas fa-rocket'></i>
+    <p class="number">211</p>
+    <p class="p-feedback">Orders Received</p>
+  </div>
+
+</div>
+
+</section>
+
+    <section class="container-screens-last">
+
+        <div class="download-iphone"></div>
+        <img class="download-iphone-img" src="media/images/download-iphone.png" alt="" width="921" height="578"
+        loading="lazy">
+        
+
+        <section class="container-screens-bottom">
+
+        <p>Do you feel like you're wasting time with small stuff instead of working towards your goals? Start
+            using
+            Sync
+            to organize your time and get a grip on your personal development</p>
+        <button class="btn download-btn" type="button"><i class="fa-brands fa-apple"></i>DOWNLOAD</button>
+        <button class="btn download-btn" type="button"><i class="fa-brands fa-google-play"></i>DOWNLOAD</button>
+
+        </section>
+
+    </section>
+
+</template>
+
+<style>
+
+.container-screens-last {
+  text-align: center;
+  width: 80vw;
+  margin: 0 auto;
+}
+
+.download-iphone {
+  margin: 0 auto 60px;
+}
+
+.download-iphone-img {
+  max-width: 100%;
+  height: auto;
+  margin-top: 140px;
+  transition: transform .2s;
+}
+
+.download-iphone-img:hover {
+  transform: scale(1.1);
+}
+.container-screens-bottom {
+  width: 80vw;
+  margin: 0 auto;
+}
+
+.container-screens-bottom p {
+  font-size: 0.9375rem;
+  font-size: clamp(0.9375rem, 0.9rem + 0.1875vw, 1.125rem);
+  color: #868687;
+  font-weight: 500;
+  padding: 2rem 0;
+  margin: 0 auto;
+  line-height: 1.6rem;
+  width: 90%;
+}
+
+
+
+</style>
+
+
+<script>
+
+</script>
