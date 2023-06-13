0fb3385e8523ac750d42c9b9bbee0201ba7c43b1
dosdedoce <dosdedoce@gmail.com>
2023-06-13 14:48:19 +0200

    Component List Default

---
+++ src/components/ComponentDefaultList.vue
@@ -0,0 +1,30 @@
+<script setup>
+     defineProps({
+     msg: {
+          type: String,
+          required: true
+          }
+     })
+
+</script>
+
+<template>
+     <!-- <ul> -->
+          <li>{{ msg }}</li>
+     <!-- </ul> -->
+</template>
+
+<style lang="css" scoped>
+     ul{
+          margin: 0;
+          padding: 0;
+          color: #ff0000;
+          font-weight: 400;
+          font-size: 0.9rem;
+          line-height: 2rem;
+          list-style-type: square;
+          list-style-position: outside;
+          padding-left: 5%;
+          margin-top: 5%;
+     }
+</style>
